4adb637327cfe9e96f7a4ac4a9a8b1ae8519cc58
ao81 <mc4@outlook.jp>
2026-04-07 00:01:08 +0900

    .vscode/tasks.json

---
+++ .vscode/arduino-upload.ps1
@@ -0,0 +1,31 @@
+param(
+    [string]$BuildPath,
+    [string]$SketchDir,
+    [string]$Port = 'COM3'
+)
+
+# path resolution
+$avrdudeRoot = "$env:LOCALAPPDATA\Arduino15\packages\arduino\tools\avrdude"
+$dirs = [System.IO.Directory]::GetDirectories($avrdudeRoot)
+$avrdudeDir = $dirs[$dirs.Length - 1] 
+
+$avrdude     = "$avrdudeDir\bin\avrdude.exe"
+$avrdudeConf = "$avrdudeDir\etc\avrdude.conf"
+$sketchName  = [System.IO.Path]::GetFileName($SketchDir)
+$hexFile     = "$BuildPath\$sketchName.ino.hex"
+
+# timer start
+$sw = [System.Diagnostics.Stopwatch]::StartNew()
+
+# build
+arduino-cli compile -q -b arduino:avr:megaADK -j 0 --build-path $BuildPath $SketchDir
+$buildTime = $sw.Elapsed.TotalSeconds
+
+# write
+$sw.Restart()
+& $avrdude -C $avrdudeConf -q -q -V -p m2560 -c wiring -P $Port -b 115200 -D -U "flash:w:${hexFile}:i"
+$uploadTime = $sw.Elapsed.TotalSeconds
+
+# output
+$totalTime = $buildTime + $uploadTime
+[Console]::WriteLine( ("`nBuild: {0:F2}s  Upload: {1:F2}s  Total: {2:F2}s" -f $buildTime, $uploadTime, $totalTime) )
