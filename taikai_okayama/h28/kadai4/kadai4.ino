d59cdde4c2d8ecb6badb6416bee6f759b77918ec
ao81 <mc4@outlook.jp>
2026-04-16 05:46:10 +0900

    taikai_okayama/h28/kadai4/kadai4.ino

--- taikai_okayama/h28/kadai4/kadai4.ino
+++ taikai_okayama/h28/kadai4/kadai4.ino
@@ -1,4 +1,4 @@
-// 16:21 phの処理が未実装
+// 29:27
 
 // js左,右をsw1,2とする
 
@@ -12,13 +12,17 @@ int Lcnt = 0, Rcnt = 0;
 int xpos, sw, tsw, ph;
 int prexpos, presw, preph;
 
-word in;
+bool isBlinking = false;
+int blinkCnt = 0;
+
+word in, tc;
 
 void onedisp(int n, int m) {
 	static int pren = 0, prem = 0;
 	if (n != pren || m != prem) {
 		pren = n; prem = m;
-		disp(num[n % 16], num[m % 16]);
+		if (n < 0 || m < 0) disp(0x00, 0x00);
+		else disp(num[n % 16], num[m % 16]);
 	}
 }
 
@@ -31,6 +35,8 @@ ISR(TIMER3_COMPA_vect) {
 		tsw = digitalRead(_USER_CON_5PIN);
 		ph = digitalRead(_USER_CON_3PIN);
 	}
+
+	if (isBlinking) tc++;
 }
 
 void setup() {
@@ -46,25 +52,50 @@ void setup() {
 }
 
 void loop() {
-	if (xpos > hi && prexpos <= hi) { // sw1
-		Lcnt += (tsw == HIGH ? -1 : 1);
-		if (Lcnt < 0) Lcnt = 0;
-		if (Lcnt > 15) Lcnt = 15;
-	}
+	if (!isBlinking) {
+		if (xpos > hi && prexpos <= hi) { // sw1
+			Lcnt += (tsw == HIGH ? -1 : 1);
+			if (Lcnt < 0) Lcnt = 0;
+			if (Lcnt > 15) Lcnt = 15;
+		}
 
-	if (xpos < lo && prexpos >= lo) { // sw2
-		Rcnt += (tsw == HIGH ? -1 : 1);
-		if (Rcnt < 0) Rcnt = 0;
-		if (Rcnt > 15) Rcnt = 15;
-	}
+		if (xpos < lo && prexpos >= lo) { // sw2
+			Rcnt += (tsw == HIGH ? -1 : 1);
+			if (Rcnt < 0) Rcnt = 0;
+			if (Rcnt > 15) Rcnt = 15;
+		}
 
-	if (sw == LOW && presw == HIGH) { // sw
-		Lcnt = Rcnt = 0;
-	}
+		if (sw == LOW && presw == HIGH) { // sw
+			Lcnt = Rcnt = 0;
+		}
 
+		if (ph == HIGH) {
+			if (Lcnt == Rcnt) {
+				onedisp(-1, -1);
+				tc = 0;
+				blinkCnt = 1;
+				isBlinking = true;
+				goto blink;
+			}
+		}
+
+		onedisp(Lcnt, Rcnt);
+
+	} else {
+	blink:
+
+		if (blinkCnt >= 6) {
+			isBlinking = false;
+
+		} else if (tc > 1000) {
+			tc = 0;
+			if (blinkCnt % 2 == 0) onedisp(-1, -1);
+			else onedisp(Lcnt, Rcnt);
+			blinkCnt++;
+		}
+	}
 
 	prexpos = xpos;
 	presw = sw;
-
-	onedisp(Lcnt, Rcnt);
+	preph = ph;
 }
