f62d71de849f2d424c85ae1c740b64c3467245f2
ao81 <mc4@outlook.jp>
2026-04-16 21:50:58 +0900

    taikai_okayama/h27/kadai1/

--- taikai_okayama/h27/kadai1/kadai1.ino
+++ taikai_okayama/h27/kadai1/kadai1.ino
@@ -1,10 +1,26 @@
+// 5:27
+
+// xpos←,→ を sw1,sw2 とする
+
 #define USE_TIMER3_ISR
 #include "mono_con.h"
 
+const int lo = 0 + 200;
+const int hi = 1023 - 200;
+
+int dp[2] = { 0x00, 0x00 };
+
 int tsw, xpos;
-int pretsw, prexpos;
 
 word in;
+
+void onedisp(int n[2]) {
+	static int pren[2] = { 0x00, 0x00 };
+	if (memcmp(n, pren, sizeof(pren)) != 0) {
+		memcpy(pren, n, sizeof(pren));
+		disp(n[0], n[1]);
+	}
+}
 
 ISR(TIMER3_COMPA_vect) {
 	if (in++ > 5) {
@@ -21,5 +37,9 @@ void setup() {
 }
 
 void loop() {
-
+	if (tsw == HIGH) dp[0] = dp[1] = 0xFF;
+	else if (tsw == LOW) dp[0] = dp[1] = 0x00;
+	if (xpos > hi) dp[0] ^= 0xFF;
+	if (xpos < lo) dp[1] ^= 0xFF;
+	onedisp(dp);
 }
