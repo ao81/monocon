3b22d53cdd9d4103ec1ee5162ea526369b85e9d0
ao81 <mc4@outlook.jp>
2026-04-28 21:29:22 +0900

    build/

---
+++ .vscode/ArduinoUploader/build/CMakeFiles/ArduinoUploader.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ao/Desktop/monokon/.vscode/ArduinoUploader/main.cpp" "CMakeFiles/ArduinoUploader.dir/main.cpp.obj" "gcc" "CMakeFiles/ArduinoUploader.dir/main.cpp.obj.d"
+  "C:/Users/ao/Desktop/monokon/.vscode/ArduinoUploader/uploader.cpp" "CMakeFiles/ArduinoUploader.dir/uploader.cpp.obj" "gcc" "CMakeFiles/ArduinoUploader.dir/uploader.cpp.obj.d"
+  "C:/Users/ao/Desktop/monokon/.vscode/ArduinoUploader/utils.cpp" "CMakeFiles/ArduinoUploader.dir/utils.cpp.obj" "gcc" "CMakeFiles/ArduinoUploader.dir/utils.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
