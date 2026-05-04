5370d422e82edbb12c4d281d1b688ebde68cf5e6
ao81 <mc4@outlook.jp>
2026-05-04 18:57:29 +0900

    apply gitignore

--- VSCode-win32-x64-1.118.1/data/cache/tmp-build/core/new.cpp.d
+++
@@ -1,4 +0,0 @@
-C:\Users\ao\Desktop\monokon_release\VSCode-win32-x64-1.118.1\data\cache\tmp-build\core\new.cpp.o: \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\new.cpp \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\new.h \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\new
--- VSCode-win32-x64-1.118.1/data/cache/tmp-build/core/wiring.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\ao\Desktop\monokon_release\VSCode-win32-x64-1.118.1\data\cache\tmp-build\core\wiring.c.o: \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\wiring.c \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\wiring_private.h \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\Arduino.h \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\binary.h \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\variants\mega/pins_arduino.h
