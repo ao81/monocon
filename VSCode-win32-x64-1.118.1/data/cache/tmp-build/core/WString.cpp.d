4bfcc226458df7d3ea51c651aec23f87cdde7085
ao81 <mc4@outlook.jp>
2026-05-06 11:57:26 +0900

    Remove cache/ from tracking

--- VSCode-win32-x64-1.118.1/data/cache/tmp-build/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\ao\Desktop\monokon_vscode\monokon\VSCode-win32-x64-1.118.1\data\cache\tmp-build\core\WString.cpp.o: \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\WString.cpp \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\WString.h
--- VSCode-win32-x64-1.118.1/data/cache/tmp-build/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\ao\Desktop\monokon_vscode\monokon\VSCode-win32-x64-1.118.1\data\cache\tmp-build\core\hooks.c.o: \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\hooks.c
