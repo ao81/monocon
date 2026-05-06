6ae137ba85f3c90ee1b8225a40df93b0da96337a
ao81 <mc4@outlook.jp>
2026-05-06 11:57:26 +0900

    Remove cache/ from tracking

--- VSCode-win32-x64-1.118.1/data/cache/tmp-build/core/wiring_analog.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\ao\Desktop\monokon_vscode\monokon\VSCode-win32-x64-1.118.1\data\cache\tmp-build\core\wiring_analog.c.o: \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\wiring_analog.c \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\wiring_private.h \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\Arduino.h \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\binary.h \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\variants\mega/pins_arduino.h
