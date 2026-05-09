a43315a729bce6db7c81b065c3ca29b6967bc06c
ao81 <mc4@outlook.jp>
2026-05-09 15:59:09 +0900

    fix uploader

--- vscode/data/daemon/build/bin/data/cache/tmp-build/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\ao\Desktop\monokon_vscode\monokon\vscode\data\daemon\build\bin\data\cache\tmp-build\core\WString.cpp.o: \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\WString.cpp \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\WString.h
--- vscode/data/daemon/build/bin/data/cache/tmp-build/core/new.cpp.d
+++
@@ -1,4 +0,0 @@
-C:\Users\ao\Desktop\monokon_vscode\monokon\vscode\data\daemon\build\bin\data\cache\tmp-build\core\new.cpp.o: \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\new.cpp \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\new.h \
- C:\Users\ao\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\new
