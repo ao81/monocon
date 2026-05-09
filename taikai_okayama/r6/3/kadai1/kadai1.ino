b9c05628afd562505150e10aa7865fb277402325
ao81 <mc4@outlook.jp>
2026-05-09 15:47:25 +0900

    taikai_okayama/r6/3/kadai1/

---
+++ taikai_okayama/r6/3/kadai1/kadai1.ino
@@ -0,0 +1,48 @@
+// sw2 -> ph
+
+#define USE_TIMER3_ISR
+#include "mono_con.h"
+
+const int speed = 100;
+int sw, ph;
+
+word in;
+
+void cw() {
+	analogWrite(FIN_PIN, speed);
+	digitalWrite(RIN_PIN, LOW);
+}
+
+void ccw() {
+	digitalWrite(FIN_PIN, LOW);
+	analogWrite(RIN_PIN, speed);
+}
+
+void stop() {
+	digitalWrite(FIN_PIN, HIGH);
+	digitalWrite(RIN_PIN, HIGH);
+}
+
+ISR(TIMER3_COMPA_vect) {
+	if (in++ > 5) {
+		in = 0;
+
+		sw = digitalRead(_USER_CON_4PIN);
+		ph = digitalRead(_USER_CON_3PIN);
+	}
+}
+
+void setup() {
+	config_init();
+	serial_init();
+
+	sw = digitalRead(_USER_CON_4PIN);
+	ph = digitalRead(_USER_CON_3PIN);
+}
+
+void loop() {
+	if (sw == LOW && ph == HIGH) stop();
+	else if (sw == LOW) ccw();
+	else if (ph == HIGH) cw();
+	else stop();
+}
