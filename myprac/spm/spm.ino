8f72a61d194a23e4177bcb79d1e16dcb36bf7317
ao81 <mc4@outlook.jp>
2026-05-22 15:42:38 +0900

    myprac/spm/

--- myprac/spm/spm.ino
+++ myprac/spm/spm.ino
@@ -1,4 +1,4 @@
-#define USE_TIMER3
+#define USE_TIMER3_ISR
 #include "mono_con.h"
 
 int tsw;
@@ -7,14 +7,15 @@ word tc;
 void cw() {
 	static int phase = 0;
 	if (--phase < 0) phase = 3;
-	lm.spm(phase).flush();
+	lm.color.SM = stepm_init(phase);
+	led_stepmotor(lm.b8);
 }
 
 ISR(TIMER3_COMPA_vect) {
 	static word in = 0;
 	if (in++ > 5) {
 		in = 0;
-		tsw = digitalRead(pin_5);
+		tsw = digitalRead(_USER_CON_5PIN);
 	}
 	tc++;
 }
@@ -23,7 +24,7 @@ void setup() {
 	config_init();
 	serial_init();
 
-	tsw = digitalRead(pin_5);
+	tsw = digitalRead(_USER_CON_5PIN);
 }
 
 void loop() {
