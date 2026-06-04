96a64843eec4ecbfb74494b9b190c734af222bf7
ao81 <mc4@outlook.jp>
2026-06-04 14:17:49 +0900

    myprac/ai/10/kadai4/

--- myprac/ai/10/kadai4/kadai4.ino
+++ myprac/ai/10/kadai4/kadai4.ino
@@ -4,6 +4,7 @@
 #include "mono_con.h"
 
 const int tempo[3] = { 1000, 500, 300 };
+int idx = 0;
 
 int x, y;
 int tsw, sw, ph;
@@ -12,7 +13,11 @@ bool swps = false;
 bool input = true;
 
 bool move = false;
-word tc;
+word tc, re;
+
+int getdir(int yy) {
+	return constrain(map(yy, 0, 1023, 0, 3), 0, 2);
+}
 
 ISR(TIMER3_COMPA_vect) {
 	static word in = 0;
@@ -20,11 +25,22 @@ ISR(TIMER3_COMPA_vect) {
 		in = 0;
 		input = true;
 	}
+	tc++;
+
+	if (re > 0) {
+		re--;
+		if (re == 0) {
+			noTone(BZ_PIN);
+			lm.led(B000);
+		}
+	}
 }
 
 void setup() {
 	config_init();
 	serial_init();
+
+	sw = presw = digitalRead(pin4);
 }
 
 void loop() {
@@ -36,13 +52,51 @@ void loop() {
 		sw = digitalRead(pin4);
 		ph = digitalRead(pin3);
 
-		if (sw == HIGH && presw == LOW) swps = false;
+		if (sw == HIGH && presw == LOW) swps = true;
 
 		presw = sw;
+	}
+
+	int i = getdir(y);
+	static int prei = 1;
+
+	if (i != prei) {
+		if (prei == 1) {
+			if (i == 0) {
+				if (idx > 0) idx--;
+			} else if (i == 2) {
+				if (idx < 2) idx++;
+			}
+		}
+		prei = i;
 	}
 
 	if (swps) {
 		swps = false;
 		move = !move;
 	}
+
+	static int cnt = 0;
+
+	if (move) {
+		if (tc >= tempo[idx]) {
+			tc = 0;
+			if (cnt == 0) {
+				tone(BZ_PIN, 2000);
+				lm.led(B001);
+			} else {
+				tone(BZ_PIN, 800);
+				lm.led(B010);
+			}
+			re = 50;
+			disp(0x00, num[cnt + 1]);
+			if (++cnt > 3) cnt = 0;
+		}
+	} else {
+		disp(0x00, 0x00);
+		cnt = 0;
+		tc = 1000;
+	}
+
+	lm.flush();
 }
