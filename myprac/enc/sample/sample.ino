b49f5524bf948f239985e91aa8d322c30ad6c4e5
ao81 <mc4@outlook.jp>
2026-06-24 22:42:24 +0900

    myprac/enc/sample/

---
+++ myprac/enc/sample/sample.ino
@@ -0,0 +1,36 @@
+#include "monocon_chuugoku.h"
+
+int sw, presw;
+bool swps = false;
+bool r = true;
+
+void isr() {
+	in.enc(p1, p2);
+
+	static word i = 0;
+	if (i++ > 50) {
+		i = 0;
+		r = true;
+	}
+}
+
+void setup() {
+	begin();
+	sw = presw = digitalRead(p4);
+}
+
+void loop() {
+	if (r) {
+		r = false;
+		sw = digitalRead(p4);
+		if (sw == LOW && presw == HIGH) swps = true;
+		presw = sw;
+	}
+
+	if (swps) {
+		swps = false;
+		c = 0;
+	}
+
+	disp(seg[c / 10], seg[c % 10]);
+}
