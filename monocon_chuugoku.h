230afe60b2ace48d6c06cd78b03e841d99e92f98
ªº <mc4@outlook.jp>
2026-06-25 11:40:22 +0900

    Update monocon_chuugoku.h

--- monocon_chuugoku.h
+++ monocon_chuugoku.h
@@ -25,6 +25,34 @@
 #define SCK_PIN  24
 #define SDI_PIN  25
 
+#define SDI_BIT  (1 << PA3)
+#define SCK_BIT  (1 << PA2)
+#define LAT1_BIT (1 << PA0)
+#define LAT2_BIT (1 << PA1)
+
+inline void fsout(uint8_t v) {
+	for (uint8_t i = 0; i < 8; i++) {
+		if (v & 0x80) PORTA |=  SDI_BIT;
+		else PORTA &= ~SDI_BIT;
+		PORTA |= SCK_BIT;
+		PORTA &= ~SCK_BIT;
+		v <<= 1;
+	}
+}
+
+inline int ar(uint8_t pin) {
+	if (pin >= A0) pin -= A0;
+	ADMUX  = (1 << REFS0) | (pin & 0x07);
+	ADCSRB = (ADCSRB & ~(1 << MUX5)) | (((pin >> 3) & 1) << MUX5);
+	ADCSRA |= (1 << ADSC);
+	while (ADCSRA & (1 << ADSC));
+	return ADC;
+}
+
+inline int dr(uint8_t pin) {
+	return (*portInputRegister(digitalPinToPort(pin)) & digitalPinToBitMask(pin)) ? HIGH : LOW;
+}
+
 const uint8_t seg[16] = {
 	0x3f, 0x06, 0x5b, 0x4f, 0x66,
 	0x6d, 0x7d, 0x27, 0x7f, 0x6f,
@@ -37,28 +65,28 @@ public:
 	int sw;
 
 	void fsw(int pin) {
-		sw = digitalRead(pin);
+		sw = dr(pin);
 	}
 
 	/* トグルスイッチ */
 	int ts;
 
 	void fts(int pin) {
-		ts = digitalRead(pin);
+		ts = dr(pin);
 	}
 
 	/* フォトインタラプタ */
 	int ph;
 
 	void fph(int pin) {
-		ph = digitalRead(pin);
+		ph = dr(pin);
 	}
 
 	/* 測距モジュール (ranging module) */
 	int rm;
 
 	void frm(int pin) {
-		rm = analogRead(pin);
+		rm = ar(pin);
 	}
 
 	/* ロータリーエンコーダ */
@@ -68,8 +96,8 @@ public:
 	uint8_t st = 0;
 
 	void fen(int pinA, int pinB, bool d = true) {
-		a = digitalRead(pinA);
-		b = digitalRead(pinB);
+		a = dr(pinA);
+		b = dr(pinB);
 
 		static const uint8_t table[7][4] = {
 			{ 0x00, 0x02, 0x04, 0x00 },
@@ -101,7 +129,7 @@ public:
 	int p = 0;
 
 	void fpr(int pin) {
-		p = analogRead(pin);
+		p = ar(pin);
 	}
 };
 In in;
@@ -113,11 +141,11 @@ int& rm = in.rm;
 int& c = in.c;
 int& p = in.p;
 
-void disp(char leftPat, char rightPat) {
-	digitalWrite(LAT1_PIN, LOW);
-	shiftOut(SDI_PIN, SCK_PIN, MSBFIRST, leftPat);
-	shiftOut(SDI_PIN, SCK_PIN, MSBFIRST, rightPat);
-	digitalWrite(LAT1_PIN, HIGH);
+void disp(char a, char b) {
+	PORTA &= ~LAT1_BIT;
+	fsout(a);
+	fsout(b);
+	PORTA |= LAT1_BIT;
 }
 
 void isr();
@@ -143,6 +171,8 @@ void begin(void) {
 	digitalWrite(LAT1_PIN, HIGH);
 	digitalWrite(SCK_PIN, LOW);
 	digitalWrite(SDI_PIN, LOW);
+
+	ADCSRA = (ADCSRA & ~0x07) | (1 << ADPS2);
 
 	cli();
 	TCCR3A = TCCR3B = TCNT3 = 0;
