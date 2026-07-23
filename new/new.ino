de62560dc053660af132886644ba0212bf6d299a
ao81 <mc4@outlook.jp>
2026-07-24 03:12:52 +0900

    new/new.ino

--- new/new.ino
+++ new/new.ino
@@ -1,43 +1,25 @@
+#define useir
 #include "monocon_chuugoku.h"
 
 void setup() {
 	begin();
 }
 
-Seq q;
-iv t;
+int dir = 1;
+int b = 0;
+int n = 0;
+
+void ir() {
+	n++;
+}
 
 void loop() {
-	de ph = in.d(d1);
-	de sw = in.d(d2);
-	de ts = in.d(d3);
-
-	if (sw.htol) q.next();
-	if (ph.ltoh) q.prev();
-
-	q.top();
-	if (q()) {
-		led(G);
-		dispn(12.3);
+	if (n >= 100) {
+		n = 0;
+		b += dir;
+		if (b == 100) dir = -1;
+		if (b == 0) dir = 1;
 	}
-	if (q()) {
-		led(B);
-	}
-	if (q()) {
-		static const int n[] = { nc4, nc5, nr, nf5, na5, ne5, nb4 };
-		static const int d[] = { 100, 300, 100, 100, 100, 100, 200 };
-
-		if (q.in()) {
-			mel.play(n, d, 7);
-		}
-
-		dispn(1.23);
-		led(R);
-	}
-	if (q()) {
-		led(0);
-		dispn(123);
-	}
-
-	mel.update();
+	led(R, b);
+	dp(0, 0, 0);
 }
