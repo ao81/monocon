78a9a773d1adb3c4eaaf79874e684ffb4e73caf7
ao81 <mc4@outlook.jp>
2026-07-24 02:38:18 +0900

    prac_renc/sample/

---
+++ prac_renc/sample/sample.ino
@@ -0,0 +1,49 @@
+#define USE_TIMER3
+#include "mono_con.h"
+
+int ts, sw, ph;
+int a, b;
+int pa, pb;
+bool r = true;
+
+ISR(TIMER3_COMPA_vect) {
+	static word in = 0;
+	if (in++ > 5) {
+		in = 0;
+		r = true;
+	}
+}
+
+void setup() {
+	config_init();
+	serial_init();
+
+	a = pa = digitalRead(pin1);
+	b = pb = digitalRead(pin2);
+}
+
+void loop() {
+	if (r) {
+		r = false;
+		ts = digitalRead(pin5);
+		sw = digitalRead(pin4);
+		ph = digitalRead(pin3);
+	}
+
+	a = digitalRead(pin1);
+	b = digitalRead(pin2);
+
+	static int c = 0;
+
+	if (pa == 0 && a == 1) {
+		if (b == 1) c--;
+		else c++;
+	}
+	pa = a;
+	pb = b;
+
+	if (c < 0) c += 24;
+	if (c > 23) c -= 24;
+
+	disp(num[c / 10], num[c % 10]);
+}
