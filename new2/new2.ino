563dfe6652a897931b3bc0ab4e5dca7d63e816d3
ao81 <mc4@outlook.jp>
2026-07-24 03:12:24 +0900

    new2/new2.ino

--- new2/new2.ino
+++ new2/new2.ino
@@ -5,43 +5,59 @@ void setup() {
 }
 
 void loop() {
-	de ts = in.d(d1);
+	de ts = in.d(d0);
 	if (ts.htol || ts.ltoh) {
 		led(0);
 		dispOff();
 	}
 
 	if (ts) {
-		if (!in.d(d3)) {
+		if (!in.d(d2)) {
 			dm.ccw(50);
-		} else if (!in.d(d2)) {
+		} else if (!in.d(d1)) {
 			dm.cw(50);
 		} else {
 			dm.fr();
 		}
 
-		if (in.ref(a2, 950)) {
+		if (in.ref(a1, 950)) {
 			led(R);
 		} else {
 			led(0);
 		}
 
-		de ph = in.d(d4);
+		de ph = in.d(d3);
 
 		static Seq q;
-		static Sw s;
 		q.top();
 		if (q()) {
 			if (ph.ltoh) {
 				q.next();
-				s.start();
 			}
 			led(G);
 		}
 		if (q()) {
+			static int cnt = 0;
+			static int pre = 0;
+			de dp = in.d(PH);
+			if (dp.ltoh) {
+				if (dm.now == 1) {
+					if (++cnt > 99) cnt = 0;
+					pre = 1;
+				} else if (dm.now == -1) {
+					if (--cnt < -99) cnt = 99;
+					pre = -1;
+				} else {
+					if (pre == 1) {
+						if (++cnt > 99) cnt = 0;
+					} else if (pre == -1) {
+						if (--cnt < -99) cnt = 0;
+					}
+				}
+			}
+			dispn(cnt);
+
 			if (ph.ltoh) {
-				s.stop();
-				dispn(s.ms() / 10);
 				q.next();
 			}
 			led(B);
@@ -49,17 +65,17 @@ void loop() {
 	} else {
 		static Seq q;
 
-		if (in.ltoh(d4)) {
+		if (in.ltoh(d3)) {
 			q.next();
 		}
 
 		q.top();
 		if (q()) {
 			static int i = 0;
-			if (in.htol(d2)) {
+			if (in.htol(d1)) {
 				if (++i > 5) i = 0;
 			}
-			if (in.htol(d3)) {
+			if (in.htol(d2)) {
 				if (--i < 0) i = 5;
 			}
 			led(R, i * 20);
@@ -70,13 +86,13 @@ void loop() {
 				led(0);
 			}
 			static int n = 0;
-			n = in.encLoop(a3, a4, -9, 99);
+			n = in.encLoop(a2, a3, -9, 99);
 			disp(seg[1], (n < 0 ? SEG_MINUS : (n / 10 == 0 ? 0x00 : seg[abs(n / 10)])), seg[abs(n % 10)], 20, 255, 255);
 		}
 		if (q()) {
 			static iv t;
 			if (t(100)) {
-				int n = in.sok(a1);
+				int n = in.sok(a0);
 				disp(seg[2], (n / 10 == 0 ? 0x00 : seg[n / 10]), seg[n % 10], 20, 255, 255);
 			}
 		}
