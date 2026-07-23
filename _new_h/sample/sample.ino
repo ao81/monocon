a0fc1e26e3a0b6db91423621d0611809bc3fdd7c
ao81 <mc4@outlook.jp>
2026-07-24 02:35:57 +0900

    update: _new_h/

--- _new_h/sample/sample.ino
+++ _new_h/sample/sample.ino
@@ -1,33 +1,8 @@
 #define USE_TIMER3
 #include "mono_con.h"
 
-const int speed = 100;
-const int led[4] = { B000, B001, B010, B100 };
-int idx = 0;
-int mode = 0;
-
-int tsw, sw, ph;
-int presw, preph;
-bool swps = false, phps = false;
-
-int phase = 0;
-
+int tsw, sw;
 word tc;
-
-void scw() {
-	if (--phase < 0) phase = 3;
-	lm.spm(phase);
-}
-
-void dcw() {
-	analogWrite(FIN_PIN, speed);
-	digitalWrite(RIN_PIN, LOW);
-}
-
-void dstop() {
-	digitalWrite(FIN_PIN, HIGH);
-	digitalWrite(RIN_PIN, HIGH);
-}
 
 ISR(TIMER3_COMPA_vect) {
 	static word in = 0;
@@ -36,13 +11,6 @@ ISR(TIMER3_COMPA_vect) {
 
 		tsw = digitalRead(pin5);
 		sw = digitalRead(pin4);
-		ph = digitalRead(pin3);
-
-		if (sw == LOW && presw == HIGH) swps = true;
-		if (ph == HIGH && preph == LOW) phps = true;
-
-		presw = sw;
-		preph = ph;
 	}
 
 	tc++;
@@ -53,51 +21,23 @@ void setup() {
 	serial_init();
 
 	tsw = digitalRead(pin5);
-	sw = presw = digitalRead(pin4);
-	ph = preph = digitalRead(pin3);
-
-	lm.led(led[idx]).flush();
+	sw = digitalRead(pin4);
 }
 
 void loop() {
-	static bool spm = false;
-	static bool dcm = false;
-
-	if (phps) {
-		phps = false;
-		if (++mode > 2) mode = 0;
-	}
-
-	if (mode == 0) {
-		if (swps) {
-			swps = false;
-			if (++idx > 3) idx = 0;
-			lm.led(led[idx]);
-		}
-	} else if (mode == 1) {
-		if (swps) {
-			swps = false;
-			spm = !spm;
-		}
-	} else if (mode == 2) {
-		if (swps) {
-			swps = false;
-			dcm = !dcm;
-		}
-	}
-
-	if (spm) {
-		if (tc >= 40) {
+	if (!sw) {
+		if (tc >= 30) {
 			tc = 0;
-			scw();
+
+			if (tsw == HIGH) {
+				lm.led(B100).spm(CW);
+			} else {
+				lm.led(B010).spm(CCW);
+			}
 		}
+	} else {
+		lm.led(B001).spm(STOP);
 	}
-
-	if (dcm) dcw();
-	else dstop();
-
-	if (tsw == HIGH) disp(num[mode], 0x00);
-	else disp(0x00, num[mode]);
 
 	lm.flush();
 }
