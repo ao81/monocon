01389e5ef5fc24ca556392e4a20c13be0038c3d9
ao81 <mc4@outlook.jp>
2026-07-24 02:14:37 +0900

    myprac/2/kadai5/

---
+++ myprac/2/kadai6/kada6.ino
@@ -0,0 +1,61 @@
+#define USE_TIMER3_ISR
+#include "mono_con.h"
+
+const int xseg[3][2] = {
+	{ 0x30, 0x00 },
+	{ 0x06, 0x30 },
+	{ 0x00, 0x06 },
+};
+
+const int yseg[3][2] = {
+	{ 0x08, 0x08 },
+	{ 0x40, 0x40 },
+	{ 0x01, 0x01 },
+};
+
+int x, y;
+
+word in;
+
+int getidx(int p) {
+	return map(p, 0, 1023, 2, -1);
+}
+
+void onedisp(int n[2]) {
+	static int pren[2] = { 0x00, 0x00 };
+	if (memcmp(pren, n, sizeof(pren)) != 0) {
+		memcpy(pren, n, sizeof(pren));
+		disp(n[0], n[1]);
+	}
+}
+
+ISR(TIMER3_COMPA_vect) {
+	if (in++ > 5) {
+		in = 0;
+
+		x = analogRead(A1);
+		y = analogRead(A2);
+	}
+}
+
+void setup() {
+	config_init();
+	serial_init();
+
+	x = analogRead(A1);
+	y = analogRead(A2);
+}
+
+void loop() {
+	int seg[2] = { 0x00, 0x00 };
+
+	int xi = getidx(x);
+	int yi = getidx(y);
+
+	seg[0] |= xseg[xi][0];
+	seg[1] |= xseg[xi][1];
+	seg[0] |= yseg[yi][0];
+	seg[1] |= yseg[yi][1];
+
+	onedisp(seg);
+}
