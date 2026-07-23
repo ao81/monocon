6bfa870ffed996f5df7f8706a330ea37f0e5054f
ao81 <mc4@outlook.jp>
2026-07-24 02:36:36 +0900

    myprac/ai/9/kadai3/

---
+++ myprac/ai/9/kadai3/kadai3.ino
@@ -0,0 +1,109 @@
+// 三角プロファイルが未実装
+
+#define USE_TIMER3
+#include "mono_con.h"
+
+const int angles[7] = { 0, 10, 20, 30, 40, 50, 60 };
+
+int x, y;
+int tsw, sw, ph;
+int presw;
+bool swps = false;
+
+int idx = 0;
+int angle = 0;
+int move = 0;
+bool mv = false;
+
+word tc;
+
+int getdir(int xx, int yy) {
+	int dx = xx - 511, dy = yy - 511;
+	if ((long)dx * dx + (long)dy * dy < 20000) return -1;
+	return (int)((atan2((double)dx, (double)dy) + 2 * PI + PI / 4) / (PI / 2)) % 4;
+}
+
+int getmove(int from, int to) {
+	return to - from;
+}
+
+ISR(TIMER3_COMPA_vect) {
+	static word in = 0;
+	if (in++ > 5) {
+		in = 0;
+
+		x = analogRead(pin2);
+		y = analogRead(pin1);
+		tsw = digitalRead(pin3);
+		sw = digitalRead(pin4);
+		ph = digitalRead(pin5);
+
+		if (sw == LOW && presw == HIGH) swps = true;
+
+		presw = sw;
+	}
+
+	tc++;
+}
+
+void setup() {
+	config_init();
+	serial_init();
+
+	x = analogRead(pin2);
+	y = analogRead(pin1);
+	tsw = digitalRead(pin3);
+	sw = presw = digitalRead(pin4);
+	ph = digitalRead(pin5);
+}
+
+void loop() {
+	int i = getdir(x, y);
+	static int prei = -1;
+
+	if (prei != i) {
+		if (prei == -1) {
+			if (i == 1) {
+				if (idx < 6) idx++;
+			} else if (i == 3) {
+				if (idx > 0) idx--;
+			}
+		}
+		prei = i;
+	}
+
+	disp(0x00, num[idx]);
+
+	if (tsw == HIGH) {
+		mv = false;
+		swps = false;
+	} else if (swps) {
+		swps = false;
+		move = getmove(angle, angles[idx]);
+		mv = true;
+	}
+
+	if (mv) {
+		move = getmove(angle, angles[idx]);
+
+		if (tc >= 10) {
+			tc = 0;
+			if (move > 0) {
+				move--;
+				angle++;
+				lm.spm(CW);
+			} else if (move < 0) {
+				move++;
+				angle--;
+				lm.spm(CCW);
+			}
+		}
+
+		if (move == 0) {
+			mv = false;
+			tone(BZ_PIN, 2000, 50);
+		}
+	}
+
+	lm.flush();
+}
