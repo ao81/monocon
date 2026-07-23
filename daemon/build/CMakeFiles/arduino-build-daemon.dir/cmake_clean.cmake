46748b4d2a464d33fdefdf46367c7fb6ab36716f
ao81 <mc4@outlook.jp>
2026-07-24 02:14:35 +0900

    close #5: fix errors in upload tool

---
+++ daemon/build/CMakeFiles/arduino-build-daemon.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arduino-build-daemon.dir/src/builder.cpp.obj"
+  "CMakeFiles/arduino-build-daemon.dir/src/builder.cpp.obj.d"
+  "CMakeFiles/arduino-build-daemon.dir/src/daemon_main.cpp.obj"
+  "CMakeFiles/arduino-build-daemon.dir/src/daemon_main.cpp.obj.d"
+  "CMakeFiles/arduino-build-daemon.dir/src/daemon_state.cpp.obj"
+  "CMakeFiles/arduino-build-daemon.dir/src/daemon_state.cpp.obj.d"
+  "bin/arduino-build-daemon.exe"
+  "bin/arduino-build-daemon.exe.manifest"
+  "bin/arduino-build-daemon.pdb"
+  "libarduino-build-daemon.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arduino-build-daemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ daemon/build/CMakeFiles/arduino-build-daemon.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for arduino-build-daemon.
+# This may be replaced when dependencies are built.
